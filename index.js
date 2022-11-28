d79147dcc19e760e430c749e3a441b3a52c94472
azharrahad31 <azherudinrahad@gmail.com>
2022-11-28 14:26:56 +0600

    JWT added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors=require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("First Sale project is running");
+ 
 });
 
 
@@ -19,6 +21,23 @@ app.get("/", (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jmnkad8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req,res,next){
+  
+    const authHerader= req.headers.authorization;
+    if(!authHerader){
+      return res.status(401).send('unauthorized acccess');
+    }
+    const token=authHerader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+      if (err) {
+          return res.status(403).send({ message: 'forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+  })
+  }
+
 
 async function run(){
    
@@ -73,12 +92,35 @@ async function run(){
                 const result=await productsBookingCollection.insertOne(bookingproduct)
                 res.send(result)
             })
-            app.get('/bookingproducts',async(req,res)=>{
+            app.get('/bookingproducts',verifyJWT,async(req,res)=>{
                 const email =req.query.email;
+                console.log('axccessToken',req.headers.authorization);
+                const decodedEmail = req.decoded.email;
+
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
                 const query={email:email};
                 const productBooking=await productsBookingCollection.find(query).toArray();
                 res.send(productBooking)
             })
+
+            app.get('/jwt', async(req,res)=>{
+                const email=req.query.email;
+                const query={email: email}
+                const user= await userCollection.findOne(query);
+                if(user){
+                  const token=jwt.sign({email}, process.env.STRIPE_SECRET_KEY, {expiresIn: '1h'});
+                 
+                  return res.send({accessToken: token})
+                }
+
+                res.status(403).send({accessToken:''}) 
+
+          
+              })
+
             app.post('/users',async(req,res)=>{
                 const user=req.body;
                 console.log(user)
